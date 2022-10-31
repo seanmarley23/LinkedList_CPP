21509e52523e3f16a390fda19fe88efc2cccd4f7
Sean Marley <seanmarley@Seans-MacBook-Air.local>
2022-10-31 01:19:21 +0000

    Making test cases for the different functions in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,77 @@
 using namespace std;
 
 int main(){
-    List testing;
-    testing.addNode(1);
-    testing.addNode(2);
-    testing.addNode(3);
-    testing.addNode(4);
-    testing.print(); //1,2,3,4
+    List list1;
+    list1.addNode(1);
+    list1.addNode(2);
+    list1.addNode(3);
+    list1.addNode(4);
+    cout << "List 1: " << endl;
+    list1.print(); // 1,2,3,4
     cout << endl;
-    testing.deleteNodeAtPosition(2);
-    testing.print(); //1,3,4
-    testing.deleteNodeAtPosition(1);
+
+    List list2;
+    list2.addNode(1);
+    list2.addNode(2);
+    list2.addNode(3);
+    list2.addNode(4);
+    cout << "List 2 Before: " << endl;
+    list2.print(); //1,2,3,4
     cout << endl;
-    testing.print(); //3,4
+
+    list2.addNodeAtPosition(100, 1);
+    list2.addNodeAtPosition(500,3);
+    cout << "List 2 After: " << endl;
+    list2.print();//100,1,500,2,3,4
+    cout << endl;
+
+    List list3;
+    list3.addNode(1);
+    list3.addNode(2);
+    list3.addNode(3);
+    list3.addNode(4);
+    list3.addNode(5);
+    list3.addNode(6);
+    list3.addNode(7);
+    list3.addNode(8);
+
+    cout << "List 3 Before: " << endl;
+    list3.print(); //1,2,3,4,5,6,7,8
+    cout << endl;
+
+    list3.deleteNodeWithValue(1);
+    list3.deleteNodeWithValue(3);
+    list3.deleteNodeWithValue(6);
+    list3.deleteNodeWithValue(8);
+
+    cout << "List 3 After: " << endl;
+    list3.print();//2,4,5,7
+    cout << endl;
+
+
+    List list4;
+    list4.addNode(1);
+    list4.addNode(2);
+    list4.addNode(3);
+    list4.addNode(4);
+    list4.addNode(5);
+    list4.addNode(6);
+
+    cout << "List 4 Before: " << endl;
+    list4.print(); //1,2,3,4,5,6
+    cout << endl;
+
+    list4.deleteNodeAtPosition(1);
+    cout << "List 4 After 1 deletion: " << endl;
+    list4.print();//2,3,4,5,6
+    cout << endl;
+
+    list4.deleteNodeAtPosition(4);
+    cout << "List 4 After 2 deletions: " << endl;
+    list4.print();//2,3,4,6
+    cout << endl;
+
+
     
 
 
